3b807c33372265d914b470e3ac781359ca8db223
Cyril Scetbon <cscetbon@2u.com>
2021-09-11 21:17:02 -0400

    Add first files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sattl",
+    version="0.1.0",
+    long_description_content_type="text/markdown",
+    url="https://github.com/2uinc/sattl",  # Optional
+    author="2U, Inc.",
+    author_email="developers@2u.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={"console_scripts": [
+        "sattl = sattl.cli:main"
+    ]},
+    packages=find_packages(exclude=["doc", "tests"]),
+    project_urls={
+        "Source": "https://github.com/2uinc/sattl",
+    },
+)
