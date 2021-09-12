2111f4ef10157372c01c9688c70c8da823e90fb7
Cyril Scetbon <cscetbon@2u.com>
2021-09-11 21:17:44 -0400

    Add cli and tests

---
+++ sattl/cli.py
@@ -0,0 +1,19 @@
+import click
+
+
+@click.command()
+@click.option("--org", required=True, type=str)
+@click.option("--is-sandbox", default=True, type=bool)
+@click.argument("path", required=True, type=click.Path())
+@click.version_option()
+def test(org, path, is_sandbox):
+    """Sattl runs a test suite against SF"""
+    check_is_sandbox(is_sandbox)
+
+
+def check_is_sandbox(is_sandbox):
+    pass
+
+
+if __name__ == '__main__':
+    test()
---
+++ tests/unit/test_cli.py
@@ -0,0 +1,30 @@
+from click.testing import CliRunner
+from sattl.cli import test
+from mock import patch
+
+
+def test_cli_required_params():
+    runner = CliRunner()
+    result = runner.invoke(test, ["/tmp"])
+    assert result.exit_code == 2
+    assert "Missing option '--org'" in result.output
+
+    result = runner.invoke(test, ["--org", "dom-ain"])
+    assert result.exit_code == 2
+    assert "Missing argument 'PATH'" in result.output
+
+    result = runner.invoke(test, ["--org", "dom-ain", "/tmp"])
+    assert result.exit_code == 0
+
+
+def test_cli_is_sandbox():
+    runner = CliRunner()
+    with patch("sattl.cli.check_is_sandbox") as check_is_sandbox:
+        result = runner.invoke(test, ["--org", "dom-ain", "--is-sandbox", "no", "/tmp"])
+    assert result.exit_code == 0
+    check_is_sandbox.assert_called_once_with(False)
+
+    with patch("sattl.cli.check_is_sandbox") as check_is_sandbox:
+        result = runner.invoke(test, ["--org", "dom-ain", "/tmp"])
+    assert result.exit_code == 0
+    check_is_sandbox.assert_called_once_with(True)
