d0a77cd2173be03b624409cea48009daeadc0de3
Cyril Scetbon <cscetbon@2u.com>
2021-09-24 17:54:59 -0400

    Indentation

--- tests/unit/test_salesforce.py
+++ tests/unit/test_salesforce.py
@@ -110,10 +110,18 @@ def test_salesforce_get(salesforce_connection):
     with patch("simple_salesforce.api.SFType.get_by_custom_id", query_account):
         assert sf_object.get() is True
         assert sf_object.refreshed is True
-    assert sf_object.content == {'Id': '0017A00000kkHm8QAE', 'Name': 'Mug Coffee', 'RecordTypeId': '0123t000000FkA9AAK',
-                                 'Student_Type__c': 'Enrolled', 'UUID__c': 'b2a1da8b-b68b-42b6-81e7-dc89ce6e86f0',
-                                 'University_ID__c': '100087987', 'University_Email__c': 'jdoe@test.com',
-                                 'SIS_Email__c': None, 'SIS_First_Name__c': 'Mug', 'SIS_Last_Name__c': 'Coffee'}
+    assert sf_object.content == {
+        'Id': '0017A00000kkHm8QAE',
+        'Name': 'Mug Coffee',
+        'RecordTypeId': '0123t000000FkA9AAK',
+        'Student_Type__c': 'Enrolled',
+        'UUID__c': 'b2a1da8b-b68b-42b6-81e7-dc89ce6e86f0',
+        'University_ID__c': '100087987',
+        'University_Email__c': 'jdoe@test.com',
+        'SIS_Email__c': None,
+        'SIS_First_Name__c': 'Mug',
+        'SIS_Last_Name__c': 'Coffee'
+    }
 
 
 def test_salesforce_matches(salesforce_connection):
