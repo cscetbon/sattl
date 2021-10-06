8b57e2bdfff64494fee15b8cd187780034f91b6f
Cyril Scetbon <cscetbon@2u.com>
2021-10-06 17:56:57 -0400

    Update tests

--- tests/unit/test_test_step.py
+++ tests/unit/test_test_step.py
@@ -61,20 +61,25 @@ def test_step_manifests_and_asserts(sample_test_step):
     mock_delete.assert_called_once()
 
 
-class CallFunctionPassed:
-    def __init__(self, func, seconds):
-        func()
-
-
 def test_step_fails_when_apply_fails(sample_test_step):
-    with pytest.raises(Exception), \
-         patch('sattl.test_step.RetryWithTimeout', CallFunctionPassed), \
-         patch.object(TestManifest, "apply", side_effect=[Exception, None]) as mock_apply, \
-         patch.object(TestAssert, "validate") as mock_validate:
+    sample_test_step.assertion = None
+
+    with pytest.raises(Exception), patch.object(TestManifest, "apply", side_effect=[Exception, ""]) as mock_apply:
         sample_test_step.run()
 
     mock_apply.assert_called_once()
-    mock_validate.assert_not_called()
+
+    sample_test_step.manifests = []
+
+    with pytest.raises(Exception), patch.object(TestDelete, "apply", side_effect=Exception) as mock_apply:
+        sample_test_step.run()
+
+    mock_apply.assert_called_once()
+
+
+class CallFunctionPassed:
+    def __init__(self, func, seconds):
+        func()
 
 
 def test_step_fails_when_assert_always_fails(sample_test_step):
